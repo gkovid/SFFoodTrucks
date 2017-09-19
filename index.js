618753381655fcb24febe9ce4b4dea4fe0982bbd
Kovid Gurrala <kovid4@gmail.com>
2017-09-18 21:12:13 -0700

    test results

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var map, infoWindow;
-      function initMap() {
+      function initializeMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -34.397, lng: 150.644},
           zoom: 6
