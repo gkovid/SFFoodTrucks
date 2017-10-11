d059830cb6df6521a22845bfdd4fc8edbe0baebf
Kovid Gurrala <kovid4@gmail.com>
2017-10-11 10:21:19 -0700

    new features

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ var map, infoWindow;
 
 $(window).on('load', function() {
 
-$("#map").height($(window).height() - $("#intro").height());
+  $("#map").height($(window).height() - $("#intro").height() - $("#menu").height());
 
-getListings(0);
+  getListings(0, "", "");
 
-initMap();
+  initMap();
+
+  $(".two").click(function() {
+    var text = $(this).text();
+    $( ".twoButton" ).text(text);
+
+    getListings(0, $( ".oneButton" ).text(), text);
+
+  });
+
+  $(".one").click(function() {
+    var text = $(this).text();
+    $( ".oneButton" ).text(text);
+
+    getListings(0, text, $( ".twoButton" ).text());
+
+  });
 
 });
 
@@ -46,10 +62,27 @@ function initMap() {
         infoWindow.open(map);
       }
 
-      function getListings(offset) {
+      function getListings(offset, type, permit) {
+
+        var testBool = 0;
+
+        var urlToQuery = "https://data.sfgov.org/resource/6a9r-agq8.json";
+        if (type != "" && !type.includes("Any Food Truck Type")) {
+          testBool = 1;
+          urlToQuery = urlToQuery + "?facilitytype=" + type;
+        }
+
+        if (permit != "" && !permit.includes("Any Permit Status")) {
+          if (testBool == 1) {
+            urlToQuery = urlToQuery + "&status=" + permit;
+          }
+          else {
+            urlToQuery = urlToQuery + "?status=" + permit;
+          }
+        }
 
         $.ajax({
-          url: "https://data.sfgov.org/resource/6a9r-agq8.json",
+          url: urlToQuery,
           type: "GET",
           data: {
             "$limit" : 10,
@@ -57,12 +90,35 @@ function initMap() {
             "$$app_token" : "Q4wkWXVTDAQjdY5fMw7iENcTf"
           }
         }).done(function(data) {
-          alert(data[0].applicant);
-          createTable(data);
           console.log(data);
+          console.log(urlToQuery);
+          createTable(data);
         });
       }
 
       function createTable(dataTable) {
 
+        $( "table" ).empty();
+
+        var body = "<tbody>";
+        var tr;
+        var td;
+
+        for (var i = 0; i < dataTable.length; i++) {
+          tr = tr + "<tr>";
+
+          td = "";
+
+          td = td + "<td>" + dataTable[i].applicant + "</td>";
+          td = td + "<td>" + dataTable[i].address + "</td>";
+          td = td + "<td>" + dataTable[i].facilitytype + "</td>";
+          td = td + "<td>" + dataTable[i].locationdescription + "</td>";
+          td = td + "<td>" + dataTable[i].status + "</td>";
+
+          tr = tr + td + "</tr>";
+        }
+
+        body = body + tr + "</tbody>";
+
+        $( "table" ).append(body);
       }
